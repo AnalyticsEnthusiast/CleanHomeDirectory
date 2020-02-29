387c5502b1f504ad7a39a37390326fd9523fe29d
darren <darren@PracticeServer2.example.com>
2020-02-29 22:43:28 +0000

    First working version with logging

--- CleanHomeDirectory.bash
+++ CleanHomeDirectory.bash
@@ -3,8 +3,15 @@
 # Interactive program that takes a username as an argument (or optional home directory argument from the user) and goes through each file greater than 100K, asks to remove, backup or keep.
 # Clean home directory utility
 
+# Variables
+D=$(date | tr ' ' '_' | tr ':' '_')
+NAME=""
+DIRECTORY=""
+no_args="true"
+LOG_FILE_PATH="CleanHomeDirectory_log_$D.log"
+
 help() {
-	echo "Usage: $0 [ -u User] [-h Home Directory ]" 1>$2
+	echo "Usage: $0 [ -u Username ] [-h Help Text ] [ -l Path to Directory to store log file ]" 1>&2
 }
 
 exit_abnormal() {
@@ -12,7 +19,21 @@ exit_abnormal() {
 	exit 1
 }
 
+exit_no_permissions() {
+	echo "User does not have permission to run this script."
+	echo "User must have sudo permissions."
+	exit 1
+}
+
 check_user_exists() {
+	
+	#if [[ "$1" = "-h" ]];
+	#then
+	#	echo "No username provided"
+	#	return 1
+	#fi
+
+
 	grep $1 /etc/passwd
 	if [[ $? -eq 0 ]];
 	then
@@ -25,7 +46,7 @@ check_user_exists() {
 check_home_directory_exists() {
 	# Users actual home dir
 	home_dir=$(grep $1 /etc/passwd | awk -F ':' '{ print $6 }')
-	
+
 	if [[ "$home_dir" = "$2" ]];
 	then
 		return 0
@@ -34,42 +55,9 @@ check_home_directory_exists() {
 	fi
 }
 
-NAME=
-DIRECTORY=
-
-while getops "u:h" option; do
-case ${option} in
-u )
-	NAME=$OPTARG
-	if [[ check_user_exists "$NAME" -eq 0 ]];
-	then
-		echo "User $OPTARG exists"
-	else
-		echo "User does not exist. Please provide a valid user"
-		exit 1
-	fi
-;;
-h ) 
-	DIRECTORY=$OPTARG
-	if [[ check_home_directory_exists "$NAME" "$DIRECTORY" -eq 0 ]];
-	then
-		echo "Valid Home directory: $DIRECTORY"
-	else
-		echo "Not a valid home directory for that user"
-		exit 1
-	fi
-;;
-\? )
-	exit_abnormal
-: )
-	echo "Invalid option: Script requires an argument" 1>&2
-	exit 1
-;;
-esac
-done
-
 # Print user ask text
 ask1(){
+	echo ""
 	echo "You will be given a list of files."
 	echo "Please indicate what you would like to do with this file."
 	echo "(1) Enter 'K' to Keep the file."
@@ -82,22 +70,25 @@ ask1(){
 ask2(){
 	file_name=$(echo "$1" | awk -F ' ' '{ print $9 }' | awk -F '/' '{ print $NF }')
 	path_to_file=$(echo "$1" | awk -F ' ' '{ print $9 }')
-	echo "Would you like to keep this file? -> $file_name\n"
+	size=$(echo "$1" | awk -F ' ' '{ print $5 }')
+	d=$(date)
+	echo "Would you like to keep this file? -> $path_to_file ($size)"
 	read -p "(K|B|D): " response
 	
-	if [[ ${response,,} = "K" ]];
+	if [[ "${response^}" == "K" ]];
 	then
-		echo "Keeping file"
+		echo "$d: File $path_to_file kept" | tee -a "$LOG_FILE_PATH"
 	
-	elif [[ ${response,,} = "B" ]];
+	elif [[ "${response^}" == "B" ]];
 	then
-		echo "Sending file to Backup...."
+		echo "$d: File $path_to_file moved to Backup" | tee -a "$LOG_FILE_PATH"
+		#mv "$path_to_file" /tmp/backup
 		cp "$path_to_file" /tmp/backup
 		
-	elif [[ ${response,,} = "D" ]];
+	elif [[ "${response^}" == "D" ]];
 	then
 		rm -f "$path_to_file"
-		echo "File $path_to_file Deleted"
+		echo "$d: File $path_to_file Deleted" | tee -a "$LOG_FILE_PATH"
 	else
 		echo "Not a valid argument. Keeping file by default."
 	fi
@@ -106,36 +97,102 @@ ask2(){
 
 main(){	
 	# Make a temporary backup directory in /tmp that will store files
+	rm -rf /tmp/backup 1>&2 /dev/null
 	mkdir /tmp/backup
 	
 	# Ask user what they would like to do with the files
 	FIRST=1
 	while IFS=$'\n' read -r line;
 	do
-		if [[ FIRST -eq 1 ]];
+		if [[ "$FIRST" -eq 1 ]];
 		then
 			FIRST=0
-			ask1
-			ask2 "$line"
+			ask1 
+			ask2 "$line" </dev/tty
 		else
-			ask2 "$line"
+			ask2 "$line" </dev/tty
 		fi
 	done < <(find $1 -type f -size +100k -exec ls -hl {} \;)
 	
 	# Make backup of the file in /tmp/backup
-	d=$(date | tr ' ' '_' | tr ':' '_')
-	name=$(echo "/tmp/backup_$d.tar.gz")
-	tar cvzf "$name" /tmp/backup/
+	name=$(echo "/tmp/backup_$D.tar.gz")
+	tar cvzf "$name" /tmp/backup 2> /dev/null
 	
 	# Remove backup directory to clean up
-	rm -rf /tmp/backup
+	rm -rf /tmp/backup 1> /dev/null 2> /dev/null
 }
 
+while getopts "u:l:h" option; do
 
+#Check that the user invoking the script has the correct permissions.
+#sudo -v 2> /dev/null 
+#[[ "$?" -eq 1 ]] && { exit_no_permissions; }
 
+case ${option} in
+h )
+	help
+	exit 1
+;;
+u )
+	NAME="$OPTARG"
+	#result=$(check_user_exists "$NAME")
+	#if [[ $? -eq 0 ]];
+	#then
+	#	DIRECTORY=$(grep "$NAME" /etc/passwd | awk -F ':' '{ print $6 }')
+	#	main "${DIRECTORY}"
+	#else
+	#	echo "User does not exist. Please provide a valid user"
+	#	exit 1
+	#fi
+;;
+#h ) 
+#	if [[ -z ${NAME} ]];
+#	then
+#		echo "No username provided"
+#		exit_abnormal
+#	fi
+#	DIRECTORY="$OPTARG"
+#	result=$(check_home_directory_exists "$NAME" "$DIRECTORY")
+#	if [[ $? -eq 0 ]];
+#	then
+#		echo "Valid Home directory: $DIRECTORY"
+#		main "${DIRECTORY}"
+#	else
+#		echo "Not a valid home directory for that user"
+#		exit 1
+#	fi
+#;;
+l )
+	LOG_PATH="$OPTARG"
+	LOG_FILE_PATH="$LOG_PATH/$LOG_FILE_PATH"
+	echo "Sending logs to $LOG_FILE_PATH" 
+;;
+\? )
+	echo "Script requires an argument"
+	exit_abnormal
+;;
+: )
+	echo "Invalid option: Script requires an argument" 1>&2 
+	exit_abnormal
+;;
+esac
+no_args="false"
+done
+#shift $((OPTIND -1))
 
+[[ "$no_args" = "true" ]] && { exit_abnormal; } 
 
+#Check that the user invoking the script has the correct permissions.
+sudo -v 2> /dev/null
+[[ "$?" -eq 1 ]] && { exit_no_permissions; }
 
+result=$(check_user_exists "$NAME")
 
-
-
+if [[ $? -eq 0 ]];
+        then
+                DIRECTORY=$(grep "$NAME" /etc/passwd | awk -F ':' '{ print $6 }')
+                main "${DIRECTORY}"
+        else
+                echo "User does not exist. Please provide a valid user"
+                exit 1
+fi
